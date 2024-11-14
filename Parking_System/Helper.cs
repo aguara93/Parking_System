db0bc3d13d0a63937bbe4e5fcabe603f38367d48
aguara93 <ag.daleki@gmail.com>
2024-11-14 07:55:19 +0100

    moved class Helper to separated class

---
+++ Parking_System/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_System
+{
+    // Hjälpfunktion för att skapa slumpmässiga registreringsnummer
+    static class Helper
+    {
+        private static Random random = new Random();
+        public static string GenerateRegNumber()
+        {
+            string letters = new string(Enumerable.Range(0, 3).Select(_ => (char)random.Next('A', 'Z')).ToArray());
+            string numbers = new string(Enumerable.Range(0, 3).Select(_ => (char)random.Next('0', '9')).ToArray());
+            return letters + numbers;
+        }
+    }
+}
